332de917b340be33b73e1e4ac03c4c76592e1439
Alex Vaos <simplex0@gmail.com>
2012-08-29 10:59:58 -0700

    mergetools added - araxis

--- settings/git-settings.sh
+++ settings/git-settings.sh
@@ -1,5 +1,3 @@
-git config --global user.name "Alex Vaos"
-git config --global user.email simplex0@gmail.com
 git config --global apply.whitespace nowarn
 git config --global merge.tool opendiff
 git config --global merge.prompt false
@@ -16,6 +14,10 @@ git config --global color.diff.white red reverse
 git config --global color.status.added yellow
 git config --global color.status.changed green
 git config --global color.status.untracked cyan
+
+git config --global mergetool.araxis.cmd \
+    'araxis-mergetool "$LOCAL" "$REMOTE" "$MERGED" "$BASE"'
+git config --global merge.tool araxis
 
 if [ -d /Applications/Xcode.app/Contents ]; then
   sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
