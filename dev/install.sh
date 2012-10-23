d1d9ec9c470304d9552e32ebb32a9a80680bdf51
Alex Vaos <alex.vaos@plumdistrict.com>
2012-10-23 11:05:45 -0700

    install reogranized

--- dev/install.sh
+++ dev/install.sh
@@ -2,11 +2,25 @@
 
 # Brew
 sudo chown -R `whoami` /usr/local
-brew update
+if [[ -e /usr/local/bin/brew ]]; then
+  brew update
+else
+  ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+  brew doctor
+fi
 
 # Apple GCC
 brew tap homebrew/dupes
 brew install apple-gcc42
+
+# GCC FIX FOR GEM BUILDING
+if [[ ! -e /usr/bin/g++ && -e /usr/bin/llvm-g++-4.2 ]]; then
+  ln -s /usr/bin/llvm-g++-4.2 /usr/bin/g++
+fi
+
+if [[ ! -e /usr/bin/gcc && -e /usr/bin/llvm-gcc-4.2 ]]; then
+  ln -s /usr/bin/llvm-gcc-4.2 /usr/bin/gcc
+fi
 
 # OH MY ZSH
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
@@ -16,6 +30,9 @@ curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | 
 
 # Ruby Install
 sh install-theinstall.sh
+
+# Uninstall Native Ruby
+sudo rm -r /System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/gems/1.8
 
 # Move Hashrocket
 sh move-hashrocket.sh
@@ -32,15 +49,6 @@ fi
 # curl get.pow.cx | sh
 
 # Xcode Select
-if [ -d "/Applications/Xcode.app/Contents" ]; then
+if [[ -d "/Applications/Xcode.app/Contents" ]]; then
   sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
-fi
-
-# GCC FIX FOR GEM BUILDING
-if [[ ! -f /usr/bin/g++ && -f /usr/bin/llvm-g++-4.2 ]]; then
-  ln -s /usr/bin/llvm-g++-4.2 /usr/bin/g++
-fi
-
-if [[ ! -f /usr/bin/gcc && -f /usr/bin/llvm-gcc-4.2 ]]; then
-  ln -s /usr/bin/llvm-gcc-4.2 /usr/bin/gcc
 fi
