71fde2b9a294b9ce80e3765b953cc6267f24742f
Alex Vaos <simplex0@gmail.com>
2012-11-17 09:38:46 -0800

    itunes takeover

--- packages/dropbox-takeover.sh
+++ packages/dropbox-takeover.sh
@@ -20,3 +20,23 @@ if [ ! -L ~/Desktop ]; then
 else
   echo 'Already syncing Desktop'
 fi
+
+# Takeover iTunes
+if [ ! -L ~/Music/iTunes ]; then
+  if [ -d ~/Music/iTunes ]; then
+    echo "Can't continue, iTunes folder exists ~/Music/iTunes"
+  elif [ ! -d ~/Dropbox/Music ]; then
+    echo "Can't continue, no Dropox iTunes folder exists ~/Dropbox/Music/iTunes"
+  else
+    mkdir -p ~/Music/iTunes
+    ITUNES_FILES=('Album Artwork' 'iTunes Library Extras.itdb' 'iTunes Library Genius.itdb' 'iTunes Library.itl' 'iTunes Library.xml' 'iTunes Music Library Backup.xml' 'iTunes Music Library.xml' 'Previous iTunes Libraries');
+    ITUNES_LENGTH=${#ITUNES_FILES[@]}
+    for ((i=0;i<$ITUNES_LENGTH;i++)); do
+      ln -s ~/Dropbox/Music/iTunes\ Library/"${ITUNES_FILES[$i]}" ~/Music/iTunes
+    done
+    mkdir -p ~/Music/iTunes/iTunes\ Music
+    ln -s ~/Dropbox/Music/iTunes\ Library/iTunes\ Music/Music ~/Music/iTunes/iTunes\ Music
+  fi
+else
+  echo 'Already syncing iTunes'
+fi
