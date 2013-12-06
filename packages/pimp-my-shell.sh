209631b565cf664bfd75a86a225be95f0c7d114b
Alex Vaos <simplex0@gmail.com>
2013-12-05 23:25:13 -0800

    pimp my shell cleanup

--- packages/pimp-my-shell.sh
+++ packages/pimp-my-shell.sh
@@ -1,9 +1,13 @@
-if [[ -f ~/.profile ]]; then
-  echo '~/.profile exists.  please remove or rename to continue'
-  exit 1
-fi
+#!/usr/bin/env sh
 
-if [[ -d ~/.oh-my-zsh ]]; then
+if [[ ! -e ~/.oh-my-zsh ]]; then
+  read -p "Install ZSH? y/n" install_zsh
+
+  # Install ZSH
+  if [[ $install_zsh = 'y' ]]; then
+    curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+  fi
+else
   # Install Theme
   rm -rf ~/.oh-my-zsh/themes/kriogenx.zsh-theme
   ln -s ~/Sites/kriogenx/scripts/configs/kriogenx.zsh-theme ~/.oh-my-zsh/themes/kriogenx.zsh-theme
@@ -20,11 +24,15 @@ fi
 echo 'source ~/.profile' >> ~/.zshrc
 echo 'source ~/.profile' >> ~/.bashrc
 
-echo '#!/bin/bash' > ~/.profile
-echo 'source ~/Sites/kriogenx/scripts/configs/bashrc' > ~/.profile
-echo 'si' >> ~/.profile
-echo '# Customize Here' >> ~/.profile
+if [[ ! -e ~/.profile ]]; then
+  echo '#!/usr/bin/env sh' > ~/.profile
+fi
+if [[ -e ~/.profile ]] && [[ ! `cat ~/.profile` =~ 'kriogenx' ]]; then
+  echo 'source ~/Sites/kriogenx/scripts/configs/bashrc' >> ~/.profile
+  echo 'si' >> ~/.profile
+  echo '# Customize Here' >> ~/.profile
+fi
 
-if [[ ! -f ~/.bash_profile ]] || [[ -z `cat ~/.bash_profile | grep '~/.profile' ]]; then
+if [[ ! -f ~/.bash_profile ]] || [[ -z `cat ~/.bash_profile | grep '~/.profile'` ]]; then
   echo 'source ~/.profile' >> ~/.bash_profile
 fi
