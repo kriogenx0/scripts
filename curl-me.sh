71757c96fb51f4b81a3f8b8a09bc7b64e798c9a2
Alex Vaos <avaos@guidebook.com>
2015-04-03 09:17:52 -0700

    curl-me messaging improved

--- curl-me.sh
+++ curl-me.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # TO USE THIS SCRIPT, RUN THIS IN TERMINAL
-# curl https://raw.githubusercontent.com/kriogenx0/scripts/master/curl-me.sh | sh
+# curl https://raw.githubusercontent.com/kriogenx0/scripts/master/curl-me.sh | bash
 
 kriogenx_dir=$HOME/Sites/kriogenx
 
@@ -14,11 +14,13 @@ if [[ `sw_vers` =~ 'Mac OS X' ]]; then
 
   # Install Homebrew
   if type brew >/dev/null 2>&1; then
+    msg 'Updating Homebrew...'
     brew update
   else
     msg 'Installing Homebrew...'
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   fi
+  msg 'Brew Doctor...'
   brew doctor
 
   # Git
@@ -41,13 +43,16 @@ if type git >/dev/null 2>&1; then
     msg 'Installing Kriogenx Scripts...'
     mkdir -p "$kriogenx_dir"
     git clone git://github.com/kriogenx0/scripts.git "$kriogenx_dir"/scripts
+    msg 'Kriogenx added to ~/Sites/kriogenx/sites'
   else
     cd "$kriogenx_dir"/scripts
     git pull --rebase
   fi
 
   # Run
-  [[ -e "$kriogenx_dir" ]] && bash "$kriogenx_dir"/scripts/install.sh
+  if [[ -e "$kriogenx_dir" ]]; then
+    bash "$kriogenx_dir"/scripts/install.sh
+  fi
 else
   msg 'Could not install kriogenx!'
   echo 'Git not found'
