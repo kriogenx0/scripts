aa2228dbaaa4adf8d2ab3624e10756258ab3cf68
Alex Vaos <avaos@mdsol.com>
2016-10-10 19:24:45 -0700

    git url changed to https

--- curl-me.sh
+++ curl-me.sh
@@ -13,9 +13,9 @@ if type git >/dev/null 2>&1; then
   # Kriogenx
   if [[ ! -d "$kriogenx_dir" ]]; then
     msg 'Installing Kriogenx Scripts...'
-    git clone git://github.com/kriogenx0/scripts.git "$kriogenx_dir"
+    git clone https://github.com/kriogenx0/scripts.git "$kriogenx_dir"
     msg "Kriogenx installed at $kriogenx_dir"
-    
+
     # Run
     if [[ -e "$kriogenx_dir" ]]; then
       msg "Kriogenx is in!"
