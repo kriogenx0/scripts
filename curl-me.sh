3b4f310e24e688f02f4ed415816768729a7f36eb
Alex Vaos <simplex0@gmail.com>
2016-10-26 18:45:24 -0700

    curl fixed

--- curl-me.sh
+++ curl-me.sh
@@ -6,7 +6,7 @@
 kriogenx_dir=$HOME/.kriogenx
 
 msg() {
-  echo -e "\033[36m$1\033[0m"
+  printf "\033[36m$1\033[0m"
 }
 
 if type git >/dev/null 2>&1; then
@@ -14,11 +14,10 @@ if type git >/dev/null 2>&1; then
   if [[ ! -d "$kriogenx_dir" ]]; then
     msg 'Installing Kriogenx Scripts...'
     git clone https://github.com/kriogenx0/scripts.git "$kriogenx_dir"
-    msg "Kriogenx installed at $kriogenx_dir"
 
     # Run
     if [[ -e "$kriogenx_dir" ]]; then
-      msg "Kriogenx is in!"
+      msg "Kriogenx installed at $kriogenx_dir"
       msg "To get started, run kriogenx install:"
       msg "bash $kriogenx_dir"/install.sh
     fi
@@ -32,6 +31,7 @@ if type git >/dev/null 2>&1; then
     msg "Installing ~/.profile"
     export PATH=$PATH:"$kriogenx_dir"/bin
     source "$kriogenx_dir"/bin/profile-scripts
+    source ~/.profile
   fi
 else
   msg 'Could not install kriogenx!'
